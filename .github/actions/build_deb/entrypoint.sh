6770dece97c5b45bf45b093fddf19c0e86b29445
Mattia Dei Rossi <deirossi@automationware.it>
2024-10-24 14:12:22 +0200

    update entrypoint

--- .github/actions/build_deb/entrypoint.sh
+++ .github/actions/build_deb/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # Create a .deb package
 rosdep update && \
+rosdep install --from-paths . --ignore-src -r -y --rosdistro=humble && \
 source /opt/ros/humble/setup.sh && \
 bloom-generate rosdebian && \
 fakeroot debian/rules binary
