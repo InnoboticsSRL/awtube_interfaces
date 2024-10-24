ebe50a1501589391a357c0970b5edb882e82bae2
Mattia Dei Rossi <deirossi@automationware.it>
2024-10-24 13:58:53 +0200

    build deb

--- .github/actions/build_deb/entrypoint.sh
+++ .github/actions/build_deb/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
+# Create a .deb package
+source /opt/ros/humble/setup.sh && \
+cd src/awtube_interfaces && \
+bloom-generate rosdebian && \
+fakeroot debian/rules binary
+
 # Move any generated .deb packages to the GitHub workspace
-mv /ws/src/*.deb /github/workspace/
+mv ../*.deb /github/workspace/deb
